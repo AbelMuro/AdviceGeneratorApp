e4927200f6566c4055a19c5595f80c645b608ad5
abelmuro <abelmuro93@gmail.com>
2024-03-16 23:57:09 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {useEffect} from 'react';
 import {Overlay, Container, Title, Advice, DividerIcon} from './styles.js';
-//import SvgIcon from './icons/pattern-divider-mobile.svg';
+import { SvgXml } from 'react-native-svg';
 
 /*  https://api.adviceslip.com  */
 
 function App() {
+
+  let icon = `<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+  <g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`
+
   return(
       <Overlay>
             <Container>
@@ -12,7 +16,9 @@ function App() {
               <Advice>
                   “It is easy to sit up and take notice, what's difficult is getting up and taking action.”
               </Advice>
-            </Container>            
+            <SvgXml xml={icon} width='200px' height='100px'/>                      
+            </Container>  
+  
       </Overlay>
     )
 }
